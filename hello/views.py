2142c063e1fe5fb868273b24dfb3070948598634
beatz4 <beatz4@naver.com>
2019-12-25 17:44:51 +0900

    'doneTutorial'

--- hello/views.py
+++ hello/views.py
@@ -6,9 +6,21 @@ import re
 from datetime import datetime
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.shortcuts import redirect
+from hello.forms import LogMessageForm
+from hello.models import LogMessage
+from django.views.generic import ListView
 
-def home(request):
-    return HttpResponse("Hello Django!")
+class HomeListView(ListView):
+    """Renders the home page, with a list of all messages."""
+    model = LogMessage
+
+    def get_context_data(self, **kwargs):
+        context = super(HomeListView, self).get_context_data(**kwargs)
+        return context
+
+# def home(request):
+#     return render(request, "hello/home.html")
 
 def hello_there(request, name):
     return render (
@@ -19,6 +31,26 @@ def hello_there(request, name):
             'date': datetime.now()
         }
     )
+
+def about(request):
+    return render(request, "hello/about.html")
+
+def contact(request):
+    return render(request, "hello/contact.html")
+
+def log_message(request):
+    form = LogMessageForm(request.POST or None)
+
+    if request.method == "POST":
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.log_date = datetime.now()
+            message.save()
+            return redirect("home")
+    else:
+        return render(request, "hello/log_message.html", {"form": form})
+
+
 
     # now = datetime.now()
     # formatted_now = now.strftime("%A, %d %B, %Y at %X")
